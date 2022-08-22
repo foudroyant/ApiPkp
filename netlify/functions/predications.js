9b10e67afef2712f448431c2d61afe8aa353c287
Foudroyant <stephanebazebibouta@gmail.com>
2022-08-22 08:22:10 +0000

    Premier commit

---
+++ netlify/functions/predications.js
@@ -0,0 +1,29 @@
+const { json } = require('stream/consumers');
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('matth25v6_fr.db');
+
+exports.handler= async function(event,context){ 
+
+    const data=getPredications().then(res=>{
+        return res 
+    })
+    
+    return{
+        statusCode:200,
+        body:JSON.stringify(await data)
+    } 
+    
+}
+
+const getPredications=()=>{
+    const promesse=new Promise((resolve, reject)=>{
+        db.all("SELECT * FROM predication", (err, rows) => {
+            //console.log(rows);
+            resolve(rows)
+        });
+        db.close();
+    })
+    return promesse
+    
+}
